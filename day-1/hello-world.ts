05cef4c725c03729a46f5ae1dc873f24fb834de1
HerrLinks <atti.szucs85@gmail.com>
2023-05-17 13:53:42 +0200

    hello world practice done

---
+++ day-1/hello-world.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Modify this program to greet you instead of the World!
+
+const name: string = 'Attila';
+
+function helloMe(): void {
+  console.log(`Hello, ${name}!`);
+}
+
+helloMe();
+
+export { helloMe }
