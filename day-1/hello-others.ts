845f3e8f68fe848a58cae318f824a6795cf04f14
HerrLinks <atti.szucs85@gmail.com>
2023-05-17 14:13:38 +0200

    hello others created

---
+++ day-1/hello-others.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Greet 3 of your classmates with this program in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+function helloOthers(): void {
+ 
+
+}
+
+helloOthers();
+
+export { helloOthers }
