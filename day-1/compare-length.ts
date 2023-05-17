192bae88bfa708e9cc110f2b606540730f6812f7
HerrLinks <atti.szucs85@gmail.com>
2023-05-17 16:19:38 +0200

    compare length is created

---
+++ day-1/compare-length.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+// - Create an array variable named `firstArrayOfNumbers`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `secondArrayOfNumbers`
+//   with the following content: `[4, 5]`
+// - Print "secondArrayOfNumbers is longer" if `secondArrayOfNumbers`
+//   has more elements than `firstArrayOfNumbers`
+// - Otherwise print: "firstArrayOfNumbers is the longer one"
+
+function compareLength(): void {
+  
+}
+
+compareLength();
+
+export { compareLength };
