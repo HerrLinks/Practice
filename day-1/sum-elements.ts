80c4d6db7c00db47a275e7c91ae577a1af142928
HerrLinks <atti.szucs85@gmail.com>
2023-05-17 16:23:10 +0200

    sum-elements.ts created

---
+++ day-1/sum-elements.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+// - Create an array variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+function sumElements(): void {
+  // Write your code here:
+}
+
+sumElements();
+
+export { sumElements };
