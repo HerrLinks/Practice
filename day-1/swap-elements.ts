534a1598e6e4fc4d030bc83f646051e870ab0608
HerrLinks <atti.szucs85@gmail.com>
2023-05-17 16:27:47 +0200

    swap elemenmt created

---
+++ day-1/swap-elements.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+// - Create an array variable named `orders`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `orders` programmatically
+// - Print the swapped array into the console:
+//   [third, second, first]
+
+function swapElements(): void {
+  // Write your code here:
+}
+
+swapElements();
+
+export { swapElements };
