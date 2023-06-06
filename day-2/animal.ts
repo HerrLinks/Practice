a20a80bdfd9de4dc2038b2f1f364c4aa31042a02
HerrLinks <atti.szucs85@gmail.com>
2023-06-06 17:49:45 +0200

    animal class created

--- day-2/animal.ts
+++ day-2/animal.ts
@@ -6,3 +6,26 @@
 //  - every animal can eat() which decreases its hunger by one
 //  - every animal can drink() which decreases its thirst by one
 //  - every animal can play() which increases both its hunger and thirst by one
+
+export class Animal {
+  hunger: number;
+  thirst: number;
+
+  constructor(hunger: number = 50, thirst: number = 50) {
+      this.hunger = hunger;
+      this.thirst = thirst;
+  }
+
+  eat() {
+      this.hunger--;
+  }
+
+  drink() {
+      this.thirst--;
+  }
+
+  play() {
+      this.hunger++;
+      this.thirst++;
+  }
+}
