36f937bd6d79483b098f872681580345803263ad
HerrLinks <atti.szucs85@gmail.com>
2023-06-06 17:30:38 +0200

    blog post created

---
+++ day-2/blog-post.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+// Create a BlogPost class that has the following properties:
+//  - authorName
+//  - title
+//  - text
+//  - publicationDate
+//
+// Make sure to set the default values of properties/fields to be an empty string
+//
+// Create a few BlogPost instances and set their properties according to the infos below:
+//  - John Doe created a post at "2000.05.04." with the title "Lorem Ipsum". It's contents are:
+//      "Lorem ipsum dolor sit amet"
+//  - Tim Urban created a post at "2010.10.10." with the title "Wait, but why?" with the following content:
+//      "A popular long-form, stick-figure-illustrated blog about almost everything."
+//  - William Turton wrote "One Engineer Is Trying to Get IBM to Reckon With Trump" at "2017.03.28."
+//      "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the
+//       center of attention. When I asked to take his picture outside one of IBM’s
+//       New York City offices, he told me that he wasn’t really into the whole
+//       organizer profile thing."
