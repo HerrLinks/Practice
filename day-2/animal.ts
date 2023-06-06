5d075f7e4aca83fa29e9b1e0da5f42b1d96f65ff
HerrLinks <atti.szucs85@gmail.com>
2023-06-06 17:48:36 +0200

    animal created

---
+++ day-2/animal.ts
@@ -0,0 +1,8 @@
+"use strict";
+
+// Create an Animal class based on the following description
+//  - every animal has a "hunger" and "thirst" property, both of them are numbers
+//  - when an animal is instantiated both hunger and thirst are set to 50
+//  - every animal can eat() which decreases its hunger by one
+//  - every animal can drink() which decreases its thirst by one
+//  - every animal can play() which increases both its hunger and thirst by one
